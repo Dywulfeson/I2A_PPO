b3c6f780813e5c1696300defe649d382626533c6
Dywulfeson <simonharp@gmx.de>
2022-06-24 12:43:39 +0200

    UPLOAD

--- PPO_ALG/ppo.py
+++ PPO_ALG/ppo.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=random.randint(0,100))
     parser.add_argument('--cpu', type=int, default=8)
-    parser.add_argument('--steps', type=int, default=8000)
+    parser.add_argument('--steps', type=int, default=32000)
     parser.add_argument('--epochs', type=int, default=400)
     parser.add_argument('--exp_name', type=str, default='ppo_8000_time_pen')
     parser.add_argument('--output_dir', type=str, default= 'data/ppo_for_eval')
